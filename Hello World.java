1ceb3b1b6ebc5fa7e9e0aac58c2e8c270b1e4c79
gbajaj20 <53325129+gbajaj20@users.noreply.github.com>
2019-08-26 14:54:48 -0400

    Creating Hello World - Java

---
+++ Hello World.java
@@ -0,0 +1,13 @@
+/******************************************************************************
+ *  1321 - Fall - 2019
+ *  Hello World.java
+ ******************************************************************************/
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+        System.out.println("Hello, World");
+    }
+
+}
